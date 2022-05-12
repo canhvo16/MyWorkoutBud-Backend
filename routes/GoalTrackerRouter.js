a5378cf8ee74b8fc2cf9328b0d56a6c1439f99b8
canhvo16 <canhvo16@gmail.com>
2022-05-12 14:39:26 -0500

    goaltrackerrouter for subtracting days updated

--- routes/GoalTrackerRouter.js
+++ routes/GoalTrackerRouter.js
@@ -5,5 +5,6 @@ router.post('/create', controller.createGoalTracker)
 router.get('/user/:userId', controller.getUserGoalTrackers)
 router.delete('/destroy/:goalTrackerId', controller.destroyGoalTracker)
 router.put('/addDaysCompleted/:goalTrackerId', controller.addDaysCompleted)
+router.put('/minusDaysCompleted/:goalTrackerId', controller.minusDaysCompleted)
 
 module.exports = router
